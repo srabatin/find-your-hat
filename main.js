ee00a97a275f56dcaac7ea9c72a8c5bc28a4fe5f
Simon Rabatin <70947548+srabatin@users.noreply.github.com>
2021-03-10 23:16:24 +0100

    add field class

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+class Field {
+  constructor(fieldArray) {
+    this.field = fieldArray;
+  }
+  print() {
+    let rows = this.field.length;
+    //let columns = this.field[0].length;
+    let i = 0;
+    while (i < rows) {
+      console.log(this.field[i].join(""));
+      i++;
+    }
+  //  console.log(this.field[0].join("") + "\n" + this.field[1].join("") + "\n" + this.field[2].join(""));
+  //  console.log("Field has " + rows + " rows.");
+  //  console.log("Field has " + columns + " columns.");
+  }
+}
+
+const array1 =  ['*', '░', 'O'];
+const array2 = ['░', 'O', '░'];
+const array3 = ['░', '^', '░'];
+const array4 = ['░', '^', '░'];
+
+let testField = [array1, array2, array3, array4];
+
+//console.log(array1.join("") + "\n" + array2.join("") + "\n" + array3.join(""));
+
+let field1 = new Field(testField);
+
+field1.print();
